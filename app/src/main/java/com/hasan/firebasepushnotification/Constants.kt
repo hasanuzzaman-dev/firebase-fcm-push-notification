fef4f189dd266a5c823b264c1111574e58010e8e
Hasan <hasanuzzaman.dev@gmail.com>
2021-01-17 18:25:36 +0600

    complete the project

--- app/src/main/java/com/hasan/firebasepushnotification/Constants.kt
+++ app/src/main/java/com/hasan/firebasepushnotification/Constants.kt
@@ -1,9 +1,11 @@
 package com.hasan.firebasepushnotification
 
 class Constants {
+
     companion object {
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAADaY4mbw:APA91bE2Yu9jhTRrmzzEuKcSNzFhxWy8tE6Itvr7_sj3wvyoPc9IGvoh5aYMgOcFyXv4JC8m5hst6GUjOxsi_Q4yzz8Y_h70hKaXeY_U3Wa0Y9tkV6cohGVgW1Vb52sAlNxVOaWjWOTA"
+        const val SERVER_KEY = "AAAADaY4mbw:APA91bFkU-7yFpG1QKSuY-mhygrE0d3ursuNhcUsRZXHX2GOMnbwRNBxCywCfQiaNp4CUs7lkWjyou0EYdND8d-SwuA9qqc21f6c7acG_OjyFaz8HuF2QyrerjkiDXwnSEEB6i9QDlsl"
         const val CONTENT_TYPE = "application/json"
     }
+
 }
